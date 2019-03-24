316ede0a1fc8f9eb5cf3033a08f96ad3f3a06e2e
Ahmed Yehya <ahmedyehya1992@gmail.com>
2019-03-24 13:04:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmedyehya/realtimechatapp/utils/EndPoints.java
@@ -0,0 +1,9 @@
+package com.example.ahmedyehya.realtimechatapp.utils;
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "https://realtimechatap.000webhostapp.com/FcmExample/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "https://realtimechatap.000webhostapp.com/FcmExample/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "https://realtimechatap.000webhostapp.com/FcmExample/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "https://realtimechatap.000webhostapp.com/FcmExample/GetRegisteredDevices.php";
+
+}
